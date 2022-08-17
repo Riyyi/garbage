a8b6e219c48130d371988c487b271874f4700d29
Frank <fdeweers@gmail.com>
2022-08-17 22:42:44 +0200

    Emu: Add initial class

---
+++ src/emu.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include "processing_unit.h"
+
+class Emu {
+public:
+void WriteRAM(int location, int length);
+void WriteVRAM(int location, int length);
+void WriteROM(int location, int length);
+
+void ReadRAM(int location, int length);
+void ReadVRAM(int location, int length);
+void ReadROM(int location, int length);
+
+private:
+float m_frequency;
+int m_cycle = 0;
+
+uint8_t m_ram[1024];
+uint8_t m_vram[1024];
+uint8_t m_rom[1024];
+
+std::vector<ProcessingUnit> m_processing_units;
+};
