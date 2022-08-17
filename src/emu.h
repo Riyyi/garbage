38c053e2d8b8141ffdfbb1becf99671cbcd9cb3c
Riyyi <riyyi3@gmail.com>
2022-08-17 22:51:03 +0200

    Emu: Convert Emu class to singleton

--- src/emu.h
+++ src/emu.h
@@ -2,25 +2,29 @@
 
 #include <cstdint>
 #include <vector>
-#include "processing_unit.h"
 
-class Emu {
+#include "processing-unit.h"
+#include "ruc/singleton.h"
+
+class Emu : ruc::Singleton<Emu> {
 public:
-void WriteRAM(int location, int length);
-void WriteVRAM(int location, int length);
-void WriteROM(int location, int length);
+	Emu(s) {}
 
-void ReadRAM(int location, int length);
-void ReadVRAM(int location, int length);
-void ReadROM(int location, int length);
+	void WriteRAM(int location, int length);
+	void WriteVRAM(int location, int length);
+	void WriteROM(int location, int length);
+
+	void ReadRAM(int location, int length);
+	void ReadVRAM(int location, int length);
+	void ReadROM(int location, int length);
 
 private:
-float m_frequency;
-int m_cycle = 0;
+	float m_frequency;
+	int m_cycle = 0;
 
-uint8_t m_ram[1024];
-uint8_t m_vram[1024];
-uint8_t m_rom[1024];
+	uint8_t m_ram[1024];
+	uint8_t m_vram[1024];
+	uint8_t m_rom[1024];
 
-std::vector<ProcessingUnit> m_processing_units;
+	std::vector<ProcessingUnit> m_processing_units;
 };
